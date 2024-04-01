9f411fdf9c56592ab48d8c1d2eef16ab83e64edb
Marwan YOUMNI <95295521+Marwwannn@users.noreply.github.com>
2024-04-01 15:32:55 +0200

    a

--- auth/authapi.php
+++ auth/authapi.php
@@ -40,7 +40,7 @@ return false;
 
 }
 }
-function isValidToken($token,$secret){
+function isValidToken($token){
 $secret = 'secret1';
 if(is_jwt_valid(get_bearer_token(),$secret) == TRUE){
 return TRUE;
@@ -51,28 +51,32 @@ return FALSE;
 
 if($http_method == "POST"){
 if(isValidUser($data['login'], $data['mdp'])){
-$login = $data['login'];
+    $login = $data['login'];
 
-$headers = array('alg' =>'HS256','typ'=>'JWT');
-$payload = array('login' => $login, 'exp' =>(time() +600));
+    $headers = array('alg' =>'HS256','typ'=>'JWT');
+    $payload = array('login' => $login, 'exp' =>(time() +600));
 
-$jwt  = generate_jwt($headers, $payload,$secret = 'secret1');
+    $jwt  = generate_jwt($headers, $payload,$secret = 'secret1');
 
-$sql = $linkpdo->prepare('SELECT `role` FROM users WHERE `login`= :login');
-$sql->bindParam(':login',$login,PDO::PARAM_STR); //Attention au type du paramètre !
-$sql->execute();
-$result = $sql->fetch(PDO::FETCH_ASSOC);
-$res = $result['role'];
-$data['jwt'] = $jwt;
-$data['role'] = $res;
-deliver_response(200,"tien le token",$data);
+    $sql = $linkpdo->prepare('SELECT `role` FROM users WHERE `login`= :login');
+    $sql->bindParam(':login',$login,PDO::PARAM_STR); //Attention au type du paramètre !
+    $sql->execute();
+    $result = $sql->fetch(PDO::FETCH_ASSOC);
+    $res = $result['role'];
+    $data['jwt'] = $jwt;
+    $data['role'] = $res;
+    deliver_response(200,"tien le token",$data);
 }else{
-deliver_response(401,"Utilisateur inconnue");
+    deliver_response(401,"Utilisateur inconnue");
 }
-}elseif (is_jwt_valid(get_bearer_token(), 'secret1')){
-deliver_response(200,"il est valide");
 }
 else{
-deliver_response(401,"Il faut utiliser POST");
+    $verif =isValidToken(get_bearer_token());
+    if($verif == TRUE){
+        deliver_response(200,"Token valide");
+    }else{
+        deliver_response(400,"Token non valide");
+    }
+
 }
 ?>
